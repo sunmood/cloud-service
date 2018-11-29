60b03660f77958569b0b8e2a52bbf3939b808505
sunmood <1397694171@qq.com>
2018-11-29 13:55:27 +0800

    添加需要用户认证的Eureka

--- cloud-eureka-server-authenticating/src/main/java/com/sunmood/eureka/EurekaApplication.java
+++ cloud-eureka-server-authenticating/src/main/java/com/sunmood/eureka/EurekaApplication.java
@@ -3,7 +3,6 @@ package com.sunmood.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
 /**
  * Created by sunmood on 2018/11/28.
  */
