e39dbd7c10bc8ccab25a6ab2702ed395666e351e
sunmood <1397694171@qq.com>
2018-12-05 10:30:17 +0800

    使用属性自定义ribbon配置

---
+++ cloud-consumer-movie-ribbon-customizing-properties/src/main/java/com/sunmood/consumermovie/CloudConsumerApplication.java
@@ -0,0 +1,22 @@
+package com.sunmood.consumermovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CloudConsumerApplication {
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConsumerApplication.class, args);
+	}
+}
--- cloud-consumer-movie-ribbon-customizing/src/main/java/com/sunmood/consumermovie/TestConfiguration.java
+++ cloud-consumer-movie-ribbon-customizing/src/main/java/com/sunmood/consumermovie/TestConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * 使用RibbonClient，为特定name的ribbon client自定义配置。
+ * 使用@RibbonClient的configuration属性，指定Ribbon的配置类
  * Created by sunmood on 2018/12/4.
  */
 @Configuration
